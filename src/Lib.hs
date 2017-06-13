347f01f978719fdaa405ae99fc5b9f148ab296c6
Steve Gravrock <sdg@panix.com>
2017-06-13 16:40:43 -0700

    Goodbye, Fractional

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,9 +8,9 @@ module Lib
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
-data ResultUnits a = GearRatio | GearInches a | MphAtRpm Integer a
+data ResultUnits = GearRatio | GearInches Float | MphAtRpm Integer Float
 
-onePair :: Fractional a => Integer -> Integer -> ResultUnits a -> a
+onePair :: Integer -> Integer -> ResultUnits -> Float
 onePair x y GearRatio = ratio x y
 onePair x y (GearInches wheelDiameter) = wheelDiameter * ratio x y
 onePair x y (MphAtRpm rpm wheelDiameter) = 
@@ -18,14 +18,14 @@ onePair x y (MphAtRpm rpm wheelDiameter) =
         inchesPerMinute = inchesDevelopment * (realToFrac rpm) * 60
     in inchesPerMinute / 63360
 
-allPairs :: Fractional a => [Integer] -> [Integer] -> ResultUnits a -> [[a]]
+allPairs :: [Integer] -> [Integer] -> ResultUnits -> [[Float]]
 allPairs fronts rears units = map (\f -> allForFront f rears units) fronts 
 
-allForFront :: Fractional a => Integer -> [Integer] -> ResultUnits a -> [a]
+allForFront :: Integer -> [Integer] -> ResultUnits -> [Float]
 allForFront front rears units = map (\r -> onePair front r units) rears
 
-ratio :: Fractional a => Integer -> Integer -> a
+ratio :: Integer -> Integer -> Float
 ratio x y = (fromInteger x) / (fromInteger y)
 
-circumference :: Fractional a => a -> a
-circumference diameter = (realToFrac pi) * diameter
+circumference :: Float -> Float
+circumference diameter = pi * diameter
