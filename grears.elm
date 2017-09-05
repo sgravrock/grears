85b06fd2828a2279b3ee65acf10f2149543d089d
Steve Gravrock <sdg@panix.com>
2017-09-04 17:07:31 -0700

    wip

--- grears.elm
+++ grears.elm
@@ -1,6 +1,7 @@
 import Html exposing (Html, div, text, fieldset, legend, input)
 import Html.Attributes exposing (type_, class, rel, href)
 import Html.Events exposing (onInput)
+import Array
 
 
 main =
@@ -12,13 +13,13 @@ main =
 
 type alias Model =
   { front : String
-  , rear : String
+  , rear : Array.Array String
   }
 
 model : Model
 model =
   { front = ""
-  , rear = ""
+  , rear = Array.fromList (List.repeat 11 "")
   }
 
 
@@ -32,16 +33,23 @@ update msg model =
     Front s ->
       { model | front = s }
     Rear s ->
-      { model | rear = s }
+      { model | rear = Array.set 0 s model.rear }
 
 isValid : Model -> Bool
-isValid model = isValidInt model.front && isValidInt model.rear
+isValid model = isValidInt model.front && isValidInt (stringAt 0 model.rear)
 
 isValidInt : String -> Bool
 isValidInt s =
   case String.toInt s of
     Ok _ -> True
     Err _ -> False
+
+stringAt : Int -> Array.Array String -> String
+stringAt i a =
+  case Array.get i a of
+    Just s -> s
+    Nothing -> ""
+
 
 -- VIEW
 
@@ -56,9 +64,10 @@ view model =
       ]
     , fieldset [] [
         legend [] [ text "Rear gears" ]
-      , intField Rear model.rear
+      , intField Rear (stringAt 0 model.rear)
       ]
     , div [] [ text "Ratio: ", text (result model) ]
+    , div [] [ text "v: ", text (toString (String.length (stringAt 0 model.rear))) ]
     ]
 
 intField : (String -> Msg) -> String -> Html Msg
@@ -75,6 +84,6 @@ result : Model -> String
 result model =
   case String.toInt model.front of
     Err _ -> ""
-    Ok f -> case String.toInt model.rear of
+    Ok f -> case String.toInt (stringAt 0 model.rear) of
       Err _ -> ""
       Ok r -> toString (toFloat f / toFloat r)
