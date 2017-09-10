f60bd51ef4d818a70b54a0863c1bc06f57834f77
Steve Gravrock <sdg@panix.com>
2017-09-10 12:06:37 -0700

    Extracted calcluator form to its own module

--- Calculator.elm
+++ Calculator.elm
@@ -1,13 +1,12 @@
 module Calculator exposing (..)
 
-import Html exposing (Html, div, text, fieldset, legend, input, select, option, label, span)
-import Html.Attributes exposing (type_, class, rel, href, size, id, for, selected)
-import Html.Events exposing (onInput, on)
+import Html exposing (Html, div)
 import Array
 import Select
 
 
 import Types exposing (Model, ValidModel, ResultUnit(..), ValidResultUnit(..), Msg(..))
+import CalculatorForm
 import Results
 
 
@@ -79,82 +78,13 @@ setAt i value list =
 view : Model -> Html Msg
 view model =
   div []
-    [ fieldset [] [
-        legend [] [ text "Front gears" ]
-      , div [] (List.indexedMap frontGearField model.fronts)
-      ]
-    , fieldset [] [
-        legend [] [ text "Rear gears" ]
-      , div [] (List.indexedMap rearGearField model.rears)
-      ]
-    , unitSelectBox model
-    , if model.unit == GearInches then
-        label []
-          [ span [class "label"] [text "Wheel diameter in inches"]
-          , wheelSizeField model.wheelDia
-          ]
-      else
-        span [] []
+    [ CalculatorForm.view model
     , maybeResultsView model
     ]
 
-unitSelectBox : Model -> Html Msg
-unitSelectBox model =
-  let
-    units = [Ratio, GearInches]
-  in
-    label []
-    [ span [class "label"] [text "Unit"]
-    , Select.fromSelected_ units SetUnit toString unitLabel model.unit
-    ]
-
-unitLabel : ResultUnit -> String
-unitLabel u =
-  case u of
-    Ratio -> "Ratio"
-    GearInches -> "Gear inches"
     
 maybeResultsView : Model -> Html Msg
 maybeResultsView model =
   case validateModel model of
     Just validModel -> Results.view validModel
     Nothing -> div [] []
-
-frontGearField : Int -> String -> Html Msg
-frontGearField i value = intField (SetFront i) value
-
-rearGearField : Int -> String -> Html Msg
-rearGearField i value = intField (SetRear i) value
-
-intField : (String -> Msg) -> String -> Html Msg
-intField updateFunc value =
-  input [ type_ "text"
-        , onInput updateFunc
-        , class (fieldClass (value == ""  || isValidInt value))
-        , size 2
-        , Html.Attributes.value value
-        ] []
-
-wheelSizeField : String -> Html Msg
-wheelSizeField value =
-  input [ type_ "text"
-        , onInput SetWheelDia
-        , class (fieldClass (isValidFloat value))
-        , size 5
-        , Html.Attributes.value value
-        ] []
-
-fieldClass : Bool -> String
-fieldClass isValid = if isValid then "" else "invalid"
-
-isValidInt : String -> Bool
-isValidInt s =
-  case String.toInt s of
-    Ok _ -> True
-    Err _ -> False
-
-isValidFloat : String -> Bool
-isValidFloat s =
-  case String.toFloat s of
-    Ok _ -> True
-    Err _ -> False
---
+++ CalculatorForm.elm
@@ -0,0 +1,90 @@
+module CalculatorForm exposing (view)
+
+import Html exposing (Html, div, text, fieldset, legend, input, select, option, label, span)
+import Html.Attributes exposing (type_, class, size, id, for, selected)
+import Html.Events exposing (onInput)
+import Select
+
+
+import Types exposing (Model, Msg(..), ResultUnit(..))
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ fieldset [] [
+        legend [] [ text "Front gears" ]
+      , div [] (List.indexedMap frontGearField model.fronts)
+      ]
+    , fieldset [] [
+        legend [] [ text "Rear gears" ]
+      , div [] (List.indexedMap rearGearField model.rears)
+      ]
+    , unitSelectBox model
+    , if model.unit == GearInches then
+        label []
+          [ span [class "label"] [text "Wheel diameter in inches"]
+          , wheelSizeField model.wheelDia
+          ]
+      else
+        span [] []
+    ]
+
+
+frontGearField : Int -> String -> Html Msg
+frontGearField i value = intField (SetFront i) value
+
+rearGearField : Int -> String -> Html Msg
+rearGearField i value = intField (SetRear i) value
+
+intField : (String -> Msg) -> String -> Html Msg
+intField updateFunc value =
+  input [ type_ "text"
+        , onInput updateFunc
+        , class (fieldClass (value == ""  || isValidInt value))
+        , size 2
+        , Html.Attributes.value value
+        ] []
+
+wheelSizeField : String -> Html Msg
+wheelSizeField value =
+  input [ type_ "text"
+        , onInput SetWheelDia
+        , class (fieldClass (isValidFloat value))
+        , size 5
+        , Html.Attributes.value value
+        ] []
+
+fieldClass : Bool -> String
+fieldClass isValid = if isValid then "" else "invalid"
+
+
+unitSelectBox : Model -> Html Msg
+unitSelectBox model =
+  let
+    units = [Ratio, GearInches]
+  in
+    label []
+    [ span [class "label"] [text "Unit"]
+    , Select.fromSelected_ units SetUnit toString unitLabel model.unit
+    ]
+
+unitLabel : ResultUnit -> String
+unitLabel u =
+  case u of
+    Ratio -> "Ratio"
+    GearInches -> "Gear inches"
+
+
+isValidInt : String -> Bool
+isValidInt s =
+  case String.toInt s of
+    Ok _ -> True
+    Err _ -> False
+
+isValidFloat : String -> Bool
+isValidFloat s =
+  case String.toFloat s of
+    Ok _ -> True
+    Err _ -> False
+
